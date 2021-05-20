7115283ef824ae5e98956a9572c916116b85b40d
Anderson Filipe <anderson.lemobs@gmail.com>
2021-05-20 18:19:25 -0300

    Card css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,9 @@
     <section class="movies">
       <div class="movie" v-for="movie in movies" :key="movie.imdbID">
         <img class="movie__image" :src="movie.Poster" alt="movie image">
-          <div class="movie__type">{{ movie.Type }}</div>
+        <div class="movie__type">{{ movie.Type }}</div>
         <div class="movie__content">
-          <div class="movie__year">{{ movie.Year }}</div>
-          <div class="movie__tittle">{{ movie.Title }}</div>
+          <div class="movie__tittle">{{ movie.Title }} ({{ movie.Year }})</div>
         </div>
       </div>
     </section>
@@ -38,6 +37,7 @@ export default {
       try {
         const res = await axios.get(`${this.apiKey}&s=${search}`);
         this.movies = res.data.Search; 
+        console.log(this.movies)
       }catch(e) {
         console.log(e);
       }
@@ -57,8 +57,6 @@ export default {
     flex-wrap: wrap;
     padding: 5rem 0.1rem 5rem 3rem; 
 
-   
-
     @media screen and (min-width:992px) {
       padding: 5rem 0 0 5rem; 
     }
@@ -68,11 +66,12 @@ export default {
     }
 
     .movie {
-      /* position: relative; */
+      background: #3d2b1f;
+      border-radius: 2%;
       display: flex;
       flex-direction: column;
-      background: #696969;
       margin-bottom: 3rem;
+      position: relative;
 
       @media screen and (min-width:0px) {
         width: calc(100% - 1.6rem);
@@ -99,9 +98,22 @@ export default {
         margin-right: 2rem;
       }
     }
+
+    .movie__content{
+      margin-left: 1rem;
+      padding: 1rem 0 1rem 0;
+    }
     .movie__image {
       width: 100%;
       height: 310px;
+    }
+
+    .movie__type {
+      background-color: #800080;
+      font-weight: bold;
+      text-transform: uppercase;
+      padding: 10px;
+      position: absolute;
     }
   }
 </style>
